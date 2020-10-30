06d4394c5f3f98e87c15b628fb2ed95ab9f7e453
Nuno Caro <nuno.pik@gmail.com>
2020-10-30 19:43:01 +0000

    Base project structure

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>{
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23")
+}
